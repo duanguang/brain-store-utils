dbf878cf3f9d59b69b8d562fca10ace0c6544d38
duanguang <492466509@qq.com>
2019-02-28 11:29:17 +0800

    fix: 修复ts文档报错并发布版本0.1.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-namespace observablePromise{
+export declare namespace observablePromise{
     type PramsResult<T>={
         /**
          *数据状态 none 数据初始化状态
@@ -52,10 +52,65 @@ namespace observablePromise{
         clear:Function
     }
 }
-export function observablePromise(promise:null|Promise,clazz:any):observablePromise.PramsResult<clazz>
+export function observablePromise<T>(promise?:Promise<any>):observablePromise.PramsResult<T>
 
-export class ObservableTempState{
-    constructor(value:Object|Promise);
+export class ObservablePromiseModel<T>{
+    constructor(options?:Promise<any>);
+
+      /**
+         *数据状态 none 数据初始化状态
+         *pending 数据请求中
+         * resolved 数据请求完成
+         * rejected 数据请求异常
+         * @type {('none'|'pending'|'resolved'|'rejected')}
+         */
+        state:'none'|'pending'|'resolved'|'rejected'
+
+        /**
+         *数据异常时，错误信息存储 例如 rejected
+         *
+         * @type {String}
+         */
+        error:String
+
+        /**
+         *目标数据存放 如api接口请求结果
+         *
+         * @type {(T|any)}
+         */
+        value:T|any
+
+        /**
+         *是否请求中
+         *
+         * @type {boolean}
+         */
+        isPending:boolean
+
+        /**
+         *请求完成
+         *
+         * @type {boolean}
+         */
+        isResolved:boolean
+
+        /**
+         *请求异常状态
+         *
+         * @type {boolean}
+         */
+        isRejected:boolean
+
+        /**
+         *初始化数据函数
+         *
+         * @type {Function}
+         */
+        clear:Function
+}
+
+export class ObservableTempState<T>{
+    constructor(value:Object|Promise<any>);
 
      /**
          *数据状态 none 数据初始化状态
@@ -78,7 +133,7 @@ export class ObservableTempState{
          *
          * @type {(T|any)}
          */
-        value:T|any
+        value:T
 
         /**
          *是否请求中
